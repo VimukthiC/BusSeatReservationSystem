2b1d4c4c688b8fa92f062f2a9c35bc79d748b48b
Vimukthi <66357690+VimukthiC@users.noreply.github.com>
2020-09-16 01:45:41 +0530

    Update SeatBookingController.java

--- src/lk/ijse/bus_seat_reservation/controller/SeatBookingController.java
+++ src/lk/ijse/bus_seat_reservation/controller/SeatBookingController.java
@@ -29,48 +29,7 @@ public class SeatBookingController {
             }
         }
         return true;
-    }
-
-    
-
-//    public List<BusSeat> getBusSeat(String busid,String date) throws ClassNotFoundException, SQLException {
-//        Connection connection = DBConnection.getInstance().getConnection();
-//        ResultSet set = connection.prepareStatement("SELECT * FROM seat WHERE bus_id='"+busid+"' AND date='"+date+"'").executeQuery();
-//        List<BusSeat> tempList = new ArrayList();
-//        while (set.next()) {
-//            tempList.add(new BusSeat(
-//                    set.getString(1),
-//                    set.getString(2),
-//                    set.getString(3),
-//                    set.getString(4)
-//            ));
-//        }
-//        return tempList;
-//    }
-
-//    public boolean saveSeatReservation(SeatReservation r1) throws ClassNotFoundException, SQLException {
-//        Connection con=DBConnection.getInstance().getConnection();
-//        PreparedStatement stm = con.prepareStatement("INSERT INTO seat_reservation VALUES(?,?,?,?,?,?)");
-//        stm.setString(1, r1. getReservation_id());
-//        stm.setString(2, r1.getSeat_id());
-//        stm.setString(3, r1.getBooking_date());
-//        stm.setString(4, r1.getStatus());
-//        stm.setString(5, r1.getNumber_of_reseved_seat());
-//        stm.setString(6, r1.getPrice());
-//       
-//        return stm.executeUpdate()>0;
-//    }
-
-   
-
-//    public boolean setstatus(String seatId,String date) throws ClassNotFoundException, SQLException {
-//       Connection con=DBConnection.getInstance().getConnection();
-//        PreparedStatement set = con.prepareStatement("UPDATE seat SET status=? WHERE seat_id ='"+seatId+"' AND date='"+date+"'"); 
-//        set.setString(1, "Booked");
-//        
-//        return set.executeUpdate()>0;
-//    }
-    
+    }    
 }
     
 
